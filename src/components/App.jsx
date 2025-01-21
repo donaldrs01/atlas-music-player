43a53b30175d24e761a7aa35a27c2b42b395d72a
donaldrs01 <ryan.s.donaldson@gmail.com>
2025-01-21 09:10:24 -0600

    added loading skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import MusicPlayer from './MusicPlayer';
 import Footer from './Footer';
-import CurrentlyPlaying from "./CurrentlyPlaying";
-import Playlist from "./Playlist";
+import LoadingSkeleton from './Loading Skeleton';
 
 
 function App() {
   return (
     <div className="flex flex-col w-full h-screen">
+      {/* Skeleton Test
+      <LoadingSkeleton />
+      */}
       <div className="flex-grow flex flex-col">
         <MusicPlayer />
       </div>   
